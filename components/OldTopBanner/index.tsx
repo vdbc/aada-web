328934cb0ffd9d75c960ba07314c615cdafda139
Cảnh Nguyễn <apmin@Canh-Nguyen.local>
2022-11-22 20:21:17 +0700

    Add video for topbanner on home page

--- components/OldTopBanner/index.tsx
+++ components/OldTopBanner/index.tsx
@@ -30,7 +30,7 @@ function _ButtonLink({ href, children }: any) {
 
 export function DefaultTopBanner() {
   return (
-    <_View backgroundUrl="/bg_banner_default.jpg">
+    <_View backgroundUrl="/the_award.mp4">
       <_Header />
       <div style={{ height: 24 }} />
       <h1>2023</h1>
@@ -52,41 +52,43 @@ export function DefaultTopBanner() {
   );
 }
 
-export function TheAwardTopBanner() {
-  return (
-    <_View backgroundUrl="/bg_banner_the_award.jpg">
-      <_Header />
-      <div style={{ height: 24 }} />
-      <h1>2023 AADA</h1>
-      <h2>IMPACTFUL ASIA</h2>
-      <div style={{ height: 40 }} />
-      <div>
-        AADA seeks to promote an Impactful Asia in architecture design and
-        construction industry that influences not just within Asian countries
-        but also at a global arena.
-      </div>
-      <div style={{ height: 40 }} />
-      <div className={styles.wrapperLink}>
-        <_ButtonLink href={"/categories "}>
-          EXPLORE 2023 AADA CATEGORIES
-        </_ButtonLink>
-        <div className={styles.spacer} />
-      </div>
-      <div style={{ height: 40 }} />
-      <h3>{"WINNERS GALA\nFAIRMONT SINGAPORE\nJUNE 2023"}</h3>
-    </_View>
-  );
-}
+// export function TheAwardTopBanner() {
+//   return (
+//     <_View backgroundUrl="/bg_banner_the_award.jpg">
+//       <_Header />
+//       <div style={{ height: 24 }} />
+//       <h1>2023 AADA</h1>
+//       <h2>IMPACTFUL ASIA</h2>
+//       <div style={{ height: 40 }} />
+//       <div>
+//         AADA seeks to promote an Impactful Asia in architecture design and
+//         construction industry that influences not just within Asian countries
+//         but also at a global arena.
+//       </div>
+//       <div style={{ height: 40 }} />
+//       <div className={styles.wrapperLink}>
+//         <_ButtonLink href={"/categories "}>
+//           EXPLORE 2023 AADA CATEGORIES
+//         </_ButtonLink>
+//         <div className={styles.spacer} />
+//       </div>
+//       <div style={{ height: 40 }} />
+//       <h3>{"WINNERS GALA\nFAIRMONT SINGAPORE\nJUNE 2023"}</h3>
+//     </_View>
+//   );
+// }
 
 export default function _View({ backgroundUrl, children }: TopBannerProps) {
   return (
     <div className={styles.container}>
       <div className={styles.background}>
-        <Image src={backgroundUrl} alt="Background" fill />
+        <video src={backgroundUrl} autoPlay controls muted loop />
       </div>
-      <div className={styles.spacer} />
+      <div className={[styles.spacer, styles.flexAnimation].join(" ")} />
       <div className={styles.spacer}>
-        <div className={styles.contentBox}>{children}</div>
+        <div className={[styles.contentBox, styles.opacityAnimation].join(" ")}>
+          {children}
+        </div>
         <div className={styles.spacer1} />
       </div>
     </div>
