77da71386a10d843c3fdd9255c2b21a4f75ea3dd
Cảnh Nguyễn <xuancanh.hct@gmail.com>
2022-12-10 15:14:19 +0700

    Update wording for registration page

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -53,6 +53,7 @@ export default function _View() {
             yet plan for a success as judging criteria for each award category
             reflect the ageless principles that drive better business result.
           </div>
+          <br />
           <h2>A QUICK 5-MINUTE SUBMISSION</h2>
           <div>
             It takes less than 5 minutes to complete an entry submission online
@@ -60,7 +61,7 @@ export default function _View() {
           </div>
           <h2>INFORMATIVE GUIDED PROCESS</h2>
           <div>
-            Guidelines are provided within every steps of entry registration,
+            Guidelines are provided within every step of entry registration,
             including what needs to be prepared, what kind of required
             information and you can come back to finish or update your entry
             following a submission progress in your admin dashboard.
@@ -109,7 +110,7 @@ export default function _View() {
               <div>
                 Choose the suitable discipline and category for your entry. The
                 number of nomination is not limited. The categories change
-                depening on chosen discipline.
+                depending on chosen discipline.
               </div>
               <br />
               <div className={styles.buttonProcess}>
@@ -117,10 +118,10 @@ export default function _View() {
                 <div className={styles.dot} />
               </div>
               <div>
-                All fees are charged per entry to 2023 AADA. Upon finishing your
-                submission categories selection, you are directed to a payment
-                gateway that allows payment in Paypal. The invoice of Entry fee
-                will be sent in email to acknowledge your successful payment.
+                All fees are charge per entry by 2023 AADA. Upon completion of
+                your submission, you will be directed to Paypal payment gateway.
+                An e-invoice will be send to your email to acknowledge receipt
+                your successful payment.
               </div>
               <br />
               <div className={styles.buttonProcess}>
@@ -128,7 +129,7 @@ export default function _View() {
                 <div className={styles.dot} />
               </div>
               <div>
-                The 2023 AADA evaluation process is a jury-based scoring
+                The 2023 AADA evaluation process is a judge-based scoring
                 procedure, hence you are required to provide detailed
                 description of your project and business. The documents needed
                 to complete your nomination include a Project Statement, Details
@@ -140,9 +141,9 @@ export default function _View() {
         <ContentCard title={"\\\nEntry Fee"}>
           <h2>AADA SPECIAL OFFERS</h2>
           <div>
-            Take advantage of the Early Entry Discounts to nominate your work
-            before 31 Dec, 2022! The submission fees start at $180 per category
-            during the Early Entry submission phase.
+            Take advantage of the Early Entry Discount to nominate your work
+            before 31 December, 2022! The submission fees start at $180 per
+            category during the Early Entry submission phase.
           </div>
           <div className={styles.offers}>
             <OfferCard>
@@ -173,8 +174,8 @@ export default function _View() {
           <h2>VALIDATED DOCUMENTS</h2>
           <div>
             The award submission requires validated documents relating to
-            projects. It’s the evidence and proof of your work to the jury
-            panel.
+            projects. It’s the evidence and proof of your work to panel of
+            judges.
           </div>
           <h2>CONTENT PREPARATION</h2>
           <div>
@@ -187,7 +188,7 @@ export default function _View() {
           <h2>PROJECT CREDIT</h2>
           <div>
             Earning an award is an excellent internal recognition for all great
-            contribution and team-work efforts. The award is far more than a
+            contribution and teamwork efforts. The award is far more than a
             credit to business, it helps build internal credibility and
             strengthens business advantages and promotes talents for
             organization.
