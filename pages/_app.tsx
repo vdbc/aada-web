1b5e416211658eb72075dd55a7cafc6e3aceed36
Cảnh Nguyễn <xuancanh.hct@gmail.com>
2023-01-09 08:42:01 +0700

    Update Google Tag Manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,22 +17,30 @@ export default function App({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
         <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-5WP8HRD"
-        ></script>
-        <script
           dangerouslySetInnerHTML={{
             __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'GTM-5WP8HRD');
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5WP8HRD');</script>
+            <!-- End Google Tag Manager -->
           `,
           }}
         ></script>
       </Head>
       <main>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WP8HRD"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+          `,
+          }}
+        ></script>
         <Provider store={store}>
           <Component {...pageProps} />
         </Provider>
