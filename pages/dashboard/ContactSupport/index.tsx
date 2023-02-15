c51b58c59170188481568640b928d92f06cbdf75
Giang Le <giangle.evelyn@gmail.com>
2023-02-15 16:25:31 +0700

    format code

--- pages/dashboard/ContactSupport/index.tsx
+++ pages/dashboard/ContactSupport/index.tsx
@@ -7,13 +7,15 @@ function ChatWithUs() {
   return (
     <div className={styles.chatWithUseContainer}>
       <div className={styles.label}>Or chat with us at</div>
-        <div className={styles.socials}>
-      <Link href="https://facebook.com/AsiaArchitectureDesignAwards"
-      target="_blank">
-      <div className={styles.iconWrapper}>
-        <SiMessenger size={20} /> 
-      </div>
-      </Link>
+      <div className={styles.socials}>
+        <Link
+          href="https://facebook.com/AsiaArchitectureDesignAwards"
+          target="_blank"
+        >
+          <div className={styles.iconWrapper}>
+            <SiMessenger size={20} />
+          </div>
+        </Link>
       </div>
       {/* <div className={styles.iconWrapper}>
         <SiViber size={20} />
