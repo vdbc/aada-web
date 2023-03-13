101680ed9a7c18e61907e3444a1ac46aab4159b6
Trung Tran <viettrung2508@gmail.com>
2023-03-13 08:57:42 +0700

    add flag

--- pages/account/InputPhoneNumber/index.tsx
+++ pages/account/InputPhoneNumber/index.tsx
@@ -1,18 +1,42 @@
 import Image from "next/image";
 import { IoIosArrowDown } from "react-icons/io";
 import InputField, { InputProps } from "../../../components/InputField";
+import { countries } from "../../../utils/countries";
 import styles from "./styles.module.scss";
 
-function SelectLocale() {
+declare type CountryPhone = {
+  flag: string;
+  value: string;
+};
+
+function SelectLocale(props: CountryPhone) {
   return (
     <div className={styles.selectLocaleContainer}>
-      <Image src="/flag_vn.svg" alt="Flag VN" width={33} height={22} />
-      <div className={styles.value}>+84</div>
-      <IoIosArrowDown />
+      <div className={styles.values}>
+        <span>{props.flag}</span>
+        <span>{props.value}</span>
+      </div>
     </div>
   );
 }
 
 export default function _View(props: InputProps) {
-  return <InputField {...props} />;
+  return (
+    <div className={styles.wrapper}>
+      <p>Phone Number</p>
+      <div className={styles.phoneInput}>
+        <select className={styles.countryCode}>
+          {countries.map((country) => (
+            <option key={country.name}>
+              <SelectLocale flag={country.flag} value={country.value} />
+            </option>
+          ))}
+        </select>
+        <input
+          className={styles.phoneNumber}
+          placeholder="Enter your phone number"
+        />
+      </div>
+    </div>
+  );
 }
