7fbef5c02605276fb90c642c26f3826f8ebd3d9b
Cảnh Nguyễn <xuancanh.hct@gmail.com>
2023-06-30 17:35:25 +0700

    Update script deploy

--- scripts/deploy-uat.sh
+++ scripts/deploy-uat.sh
@@ -3,10 +3,12 @@ USERNAME=root
 HOSTS="150.95.111.106"
 
 rm -rf web.zip .next
+npm i
 npm run build
-
 for HOSTNAME in ${HOSTS} ; do
-    ssh -l ${USERNAME} ${HOSTNAME} "cd vdbc/aada-web && git pull && npm i && rm -rf web.zip .next"
-    scp -r .next ${USERNAME}@${HOSTNAME}:~/vdbc/aada-web
-    ssh -l ${USERNAME} ${HOSTNAME} "cd vdbc/aada-web && pm2 restart aada-web || npm run start:pm2"
+    ssh -l ${USERNAME} ${HOSTNAME} "cd vdbc/dev-ops/aada-web && git pull && rm -rf .next public"
+    scp -r web.zip ${USERNAME}@${HOSTNAME}:~/vdbc/dev-ops/aada-web
+    ssh -l ${USERNAME} ${HOSTNAME} "cd vdbc/dev-ops/aada-web && unzip -o ./web.zip"
+    ssh -l ${USERNAME} ${HOSTNAME} "cd vdbc/dev-ops/aada-web && npm run stop:pm2"
+    ssh -l ${USERNAME} ${HOSTNAME} "cd vdbc/dev-ops/aada-web && npm run start:pm2 && pm2 save"
 done
