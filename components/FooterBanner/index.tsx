3522c0d3fdaee34078787503f72f94aee6e5f6e8
Cảnh Nguyễn <xuancanh.hct@gmail.com>
2023-08-28 11:19:40 +0700

    Update UI and content advisors page

--- components/FooterBanner/index.tsx
+++ components/FooterBanner/index.tsx
@@ -14,16 +14,16 @@ function _ButtonLink({ href, children }: any) {
 
 export function TheAwardFooterBanner() {
   return (
-    <_View bgUrl="/backg_the_award.jpg">
+    <_View bgUrl="/bg_the_award.webp">
       <h1>{"Why you\nshould submit"}</h1>
       <_ButtonLink href="/categories">UNCOVER 2023 AADA CATEGORIES</_ButtonLink>
     </_View>
   );
 }
 
-export function AdvisorsFooterBanner() {
+export function AdvisorsFooterBanner({ className }: any) {
   return (
-    <_View bgUrl="/backg_the_award.jpg">
+    <_View bgUrl="/bg_the_award.webp" className={className}>
       <h1>{"Ready to\nSubmit your Work?"}</h1>
       <_ButtonLink href="/registration">NOMINATE NOW</_ButtonLink>
     </_View>
@@ -32,7 +32,7 @@ export function AdvisorsFooterBanner() {
 
 export function GetInvolvedFooterBanner() {
   return (
-    <_View bgUrl="/backg_the_award.jpg">
+    <_View bgUrl="/bg_the_award.webp">
       <h1>{"EXCITED TO BECOME\nA SPONSOR?"}</h1>
       <_ButtonLink href="">CONTACT US</_ButtonLink>
     </_View>
@@ -43,7 +43,9 @@ export function RegistrationFooterBanner({ className }: any) {
   return (
     <_View bgUrl="/bg_the_award.webp" className={className}>
       <h1>{"READY TO\nSUBMIT YOUR WORK?"}</h1>
-      <_ButtonLink href="/open-account">UNCOVER 2023 AADA CATEGORIES</_ButtonLink>
+      <_ButtonLink href="/open-account">
+        UNCOVER 2023 AADA CATEGORIES
+      </_ButtonLink>
     </_View>
   );
 }
--- pages/advisors/index.tsx
+++ pages/advisors/index.tsx
@@ -22,8 +22,7 @@ const contents: AdvisorModel[] = [
     name: "ALBERTO BERETTA (Mr.)",
     avatar: "/advisors/berretta.svg",
     country: "Italy",
-    title:
-      "Founder & Managing Partner of Italian Fit Out SRL, a competent project director in the fit out sector with over 25 years in the industry.",
+    title: "Founder & Managing Partner of Italian Fit Out SRL",
     description:
       "Founded the Interior Design Fit Out & Furniture company, Mr. Alberto Beretta has been acting as founder, managing partners regarding the production of modern and contemporary furniture, furnishing accessories for the Luxury residences, Luxury Hotels and Executive offices worldwide.",
   },
@@ -32,7 +31,7 @@ const contents: AdvisorModel[] = [
     avatar: "/advisors/mr_tan_quee_peng.jpg",
     country: "Singapore",
     title:
-      "Singapore registered architect as well as a certified Green Mark Manager, Universal Design Assessor and Design for Safety Professional, General Director at RSP Vietnam.",
+      "Certified Green Mark Manager, Universal Design Assessor and Design for Safety Professional, General Director at RSP Vietnam.",
     description:
       "Quee Peng has more than 20 years of architectural experience leading major projects across Asia. He believes that architecture is an inextricable part of our experience and has the power to transform and inform our sensibilities and perceptions, from personal spaces to our interaction with society at large. Quee Peng takes a whole-of-life cycle approach on projects, from new development to brownfield asset enhancement, to deliver quality to stakeholders at all levels.",
   },
@@ -40,8 +39,7 @@ const contents: AdvisorModel[] = [
     name: "GIANFRANCO BIANCHI (Mr.)",
     avatar: "/advisors/mr_bianchi.jpg",
     country: "Italy",
-    title:
-      "Founder, CEO of Italian Atelier - the most experienced organization in Asia-Pacific that operates across 36 countries.",
+    title: "Founder, CEO of Italian Atelier SRL",
     description:
       "Established Italian Atelier in 2008, Mr. Gianfranco Bianchi, is a leader in distributing the most prestigious luxury furniture brands in the Asia Pacific and represents over forty brands worldwide. He has always been fascinated by Asian Culture and history, and had a strong belief that Asia truly represents the future of the world economy. Italian Atelier has contributed a positive impact on the luxury lifestyle of 37 countries",
   },
@@ -49,8 +47,7 @@ const contents: AdvisorModel[] = [
     name: "WINNY MONICA OEI (Ms.)",
     avatar: "/advisors/ms_oei.jpg",
     country: "Singapore",
-    title:
-      "Chief Corporate Officer at Chiu Teng Group (CTC), Singapore - a real estate developer and constructor in industrial and commercial sector.",
+    title: "Chief Corporate Officer at Chiu Teng Group (CTC), Singapore",
     description:
       "As adviser roles she is responsible for execution of day-to-day operation of various administration functions of the companies which include to strategize financial and corporate affairs. Before joining CTC, Ms. Winny Oei held position as Chief Commercial Officer at Soil build Group who leads and monitor the Group’s Procurement and Purchasing since 2012. During her past 20 years in the Group since year 2000 , she was responsible for developing an implemented internal work processes and procedure other than to established long term strategic relationships and corporation with external Clients and Vendors.",
   },
@@ -66,20 +63,9 @@ const contents: AdvisorModel[] = [
     name: "Aticha Chareerat (Ms.)",
     avatar: "/advisors/ms_chareerat.jpg",
     country: "Thailand",
-    title:
-      "Founder of PAD Space Artisan with numerous hospitality projects across Asia.",
+    title: "Founder of PAD Space Artisan, Award-winning company, Thailand",
     description:
       "Established PAD Space Artisan in 2015, Ms.Aticha has been leading as founder and design lead. Her studio focuses on craftsmanship, they put a lot of afford into crafting from overall guest experience, branding, feature, joinery, wallpaper, sanitary, ironmongery, small props, graphics, and collateral. There is almost everything that they craft and try to minimize standard objects as much as we could. In order to make her client’s project distinctive and deserves recognition.",
-  },
-
-  {
-    name: "Thomas Thang (Mr.)",
-    avatar: "/advisors/mr_thang.jpg",
-    country: "Hong Kong",
-    title:
-      "Country Head of Mulpha International Bhd of Malaysia in Vietnam\nGeneral Manager of Gaw Capital Advisors Ltd",
-    description:
-      "Thomas has over 35 years of experience and local knowledge gained from involvement in numbers of projects including mixed use developments, hotels & resorts, commercial buildings and hospitals in Ho Chi Minh City, Danang and Hanoi has equipped him with extensive local relationships in the business cycles from North to South of Vietnam.t",
   },
   {
     name: "Chang Huai-yan (Mr.)",
@@ -88,6 +74,15 @@ const contents: AdvisorModel[] = [
     title: "Founder of Salad Dressing, Award-winning design studio, Singapore.",
     description:
       "Huai-yan is inspired by contemporary culture that is defined by information technology, genomic science and cosmic explorations. He leads a practice that leverages on the flux of humanity during this post-Anthropocene transition. The studio does works range from residential, hospitality, master planning to experiential curation and nation branding.",
+  },
+  {
+    name: "Thomas Thang (Mr.)",
+    avatar: "/advisors/mr_thang.jpg",
+    country: "Hong Kong",
+    title:
+      "Country Head of Mulpha International Bhd of Malaysia in Vietnam\nGeneral Manager of Gaw Capital Advisors Ltd",
+    description:
+      "Thomas has over 35 years of experience and local knowledge gained from involvement in numbers of projects including mixed use developments, hotels & resorts, commercial buildings and hospitals in Ho Chi Minh City, Danang and Hanoi has equipped him with extensive local relationships in the business cycles from North to South of Vietnam.t",
   },
   {
     name: "Constance Tew (Ms.)",
@@ -109,7 +104,8 @@ const contents: AdvisorModel[] = [
     name: "Cam Tu Nguyen(Ms.)",
     avatar: "/advisors/ceo.jpg",
     country: "Vietnam",
-    title: "CEO, Founder of Vietnam Design & Build Center, COO of Asia Awards Organization",
+    title:
+      "CEO, Founder of Vietnam Design & Build Center, COO of Asia Awards Organization",
     description:
       "With several years of experience in the marketing industry, including a five-year tenure in the resort real estate sector, Tu presently holds the position of CEO at D&B Holding. Additionally, she is the founder and manager of the Vietnam Design & Build Center (VDBC). In the year 2022, VDBC and AAO forged a strategic partnership characterized by comprehensive collaboration, with a long-term vision to drive robust development in Asia. This collaboration aims to recognize excellence in products, individuals, organizations, and businesses through prestigious awards.",
   },
@@ -152,22 +148,28 @@ export default function View() {
         <AdvisorsTopBanner />
         <ContentCard title={"\\\nTHE ADVISORS"}>
           <p>
-            The Board of Advisors comprises accomplished professionals from various industries and diverse backgrounds, each possessing substantial experience in fields like real estate development, architecture, hospitality, media, and academia.
+            The Board of Advisors comprises accomplished professionals from
+            various industries and diverse backgrounds, each possessing
+            substantial experience in fields like real estate development,
+            architecture, hospitality, media, and academia.
           </p>
           <p>
-            Their impressive expertise in their respective domains empowers us to offer a broad spectrum of plans that align with the expectations of our partners.
+            Their impressive expertise in their respective domains empowers us
+            to offer a broad spectrum of plans that align with the expectations
+            of our partners.
           </p>
         </ContentCard>
         <div className={styles.title}>MEET THE ADVISORS</div>
         <div className={styles.advisors}>
           {contents.map((item, index) => (
-            <AdvisorItem key={item.name} {...item} rtl={index % 2 == 1} />
+            <AdvisorItem key={item.name} {...item} rtl={index % 2 == 0} />
           ))}
         </div>
-
-        <AdvisorsFooterBanner />
       </main>
-      <Footer />
+      <footer className={styles.footer}>
+        <AdvisorsFooterBanner className={styles.footerBanner} />
+        <Footer />
+      </footer>
     </div>
   );
 }
