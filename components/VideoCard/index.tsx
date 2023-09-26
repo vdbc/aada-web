4e78de2a9de0ef4fa8c9ff39198145c5fb592589
Cảnh Nguyễn <xuancanh.hct@gmail.com>
2023-09-26 14:47:38 +0700

    Update logic display video

--- components/VideoCard/index.tsx
+++ components/VideoCard/index.tsx
@@ -1,5 +1,6 @@
 import { isEmpty } from "lodash";
 import { useEffect, useRef, useState } from "react";
+import ReactPlayer from "react-player";
 import { useAppSelector } from "../../store";
 import { selectVideoDetail } from "../../store/modules/video";
 import styles from "./styles.module.scss";
@@ -25,34 +26,33 @@ export default function VideoCard({ id, className }: AlbumCardProps) {
   useEffect(() => {
     const width = ref.current?.clientWidth || 0;
     if (!width) return;
-    setHeight((width * 9) / 16);
-    const youtubeId = getYoutubeIdFromUrl(url);
-    setSrc(`https://www.youtube.com/embed/${youtubeId}`);
+    setWidth(width);
   });
 
   const video = useAppSelector(selectVideoDetail(id)) ?? {};
   const { title, description, url } = video;
   const ref = useRef<HTMLIFrameElement>(null);
-  const [height, setHeight] = useState(300);
-  const [src, setSrc] = useState("");
-
+  const [width, setWidth] = useState(600);
+  const [playing, setPlaying] = useState(false);
 
   if (isEmpty(video)) return null;
 
   return (
-    <div
-      className={[styles.container, className].join(" ")}
-      ref={ref}
-      style={{ height }}
-    >
-      <iframe
-        className={styles.iframe}
-        title={title}
-        src={src}
-        allow="encrypted-media"
-        allowFullScreen
+    <div className={[styles.container, className].join(" ")} ref={ref}>
+      <ReactPlayer
+        width={width}
+        height={(width * 9) / 16}
+        url={url}
+        onPlay={() => setPlaying(true)}
+        // onPause={() => setPlaying(false)}
+        controls
+        playing={playing}
       />
-      <div className={styles.content}>
+      <div
+        className={styles.content}
+        style={{ display: playing ? "none" : undefined }}
+        onClick={() => setPlaying(true)}
+      >
         <div className={styles.title}>{title}</div>
         <div className={styles.desc}>{description}</div>
       </div>
